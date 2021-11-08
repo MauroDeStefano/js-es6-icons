297ea41f60f3639396007a000eaf6d753b5415c0
MauroDeStefano <maurodestefano@gmail.com>
2021-11-08 16:07:48 +0100

    milestone-3 done, whitout the autorefresh of the page

--- js/milestone-3/script.js
+++ js/milestone-3/script.js
@@ -1,2 +1,161 @@
 // **Milestone 3**
 // Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone *(animal, vegetable, user)*. Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+
+const boxContent = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const container = document.querySelector(".container");
+const valueSelection = parseInt(document.getElementById("selector").value);
+console.log(valueSelection);
+
+init();
+
+function init(){
+
+	if (valueSelection === 1){
+		createBox(boxContent);
+		console.log(createBox(boxContent));
+	} else if(valueSelection === 2){
+		const arrayAnimal = boxContent.filter((element) => element.type === 'animal');
+		createBox(arrayAnimal);
+		console.log(arrayAnimal);
+	}else if(valueSelection === 3){
+		const arrayVegetable = boxContent.filter((element) => element.type === 'vegetable');
+		createBox(arrayVegetable);
+		console.log(arrayVegetable);
+	}else{
+		const arrayUser = boxContent.filter((element) => element.type === 'user');
+		createBox(arrayUser);
+		console.log(arrayUser);
+	}
+
+}
+
+function createBox(array){
+	container.innerHTML = "";
+	array.forEach(element => {
+  const name = element.name.toUpperCase();
+  
+  container.innerHTML += `
+  <div class="box d-flex flex-column justify-content-center text-center align-items-center clearfix m-3">
+    <i class="${element.family} ${element.prefix}${element.name} mds-${element.color}"></i>
+    <span>${name}</span> 
+  </div>
+  `; 
+});
+}
+
+
+
